156474dce01b859d2c12f14f56916cd2158c1d41
e-licea <elicea0412@gmail.com>
2021-02-16 02:54:03 -0500

    ran some tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,36 @@ module.exports = {
 };
 
 function success(item) {
+
+  if(item.enhancement < 20){
+    item.enhancement = item.enhancement +1
+  }else{
+    null
+  }
+
   return { ...item };
 }
 
 function fail(item) {
+
+  if(item.durability < 15){
+    item.durability = item.durability -5
+  }else{
+    item.durability = item.durability -10
+  }
+
+  if(item.enhancement > 16 ){
+    item.enhancement = item.enhancement -1
+  }else{
+    null
+  }
+
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,61 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+//Test that will check to make sure enhancer.js is defined.
+describe('enhancer', ()=>{
+    test('Enhancer exists', ()=>{
+        expect(enhancer).toBeDefined()
+    });
+
+})
+
+describe('Enhancer methods return proper item and follow method rules', ()=>{
+
+    const Obj = {
+        "name": 'Potion' ,
+        "durability": 89,
+        "enhancement": 10 ,
+    }
+
+    test('durability restores', ()=>{
+        const expected = 100
+        expect(enhancer.repair(Obj).durability).toBe(expected)
+    })
+
+    test('when enhancement succeeds ... ', ()=>{
+        const newEnhancement = Obj.enhancement + 1
+
+        expect(enhancer.success(Obj).enhancement).toBe(newEnhancement)
+
+    })
+})
+
+describe('Item object contains a valid value for each key: name, durability, enhancement', ()=>{
+
+    let item = {
+        "name": 'Potion' ,
+        "durability": 89,
+        "enhancement": 0 ,
+    };
+
+    test('key value pairs exits', ()=>{
+        expect(item).toHaveProperty('name')
+        expect(item).toHaveProperty('durability')
+        expect(item).toHaveProperty('enhancement')
+    })
+
+    test('ensures data type values of properties are valid and within their respective range', ()=>{
+    
+        expect(typeof(item.name)).toBe('string');
+        expect(typeof(item.enhancement)).toBe('number');
+        expect(typeof(item.durability)).toBe('number');
+
+        expect(item.durability).toBeGreaterThanOrEqual(0);
+        expect(item.durability).toBeLessThanOrEqual(100);
+        expect(item.enhancement).toBeGreaterThanOrEqual(0);
+        expect(item.enhancement).toBeLessThanOrEqual(20)
+    })
+})
+
+
+//Triple AAA Arrange -> Act ->  Asses
